702dfdfb29f1821bba911e4678c404bba685d186
r1cardohj <2413302357@qq.com>
2025-05-13 23:18:00 +0800

    add command line proxy manager function

---
+++ tool.sh
@@ -0,0 +1,13 @@
+function proxy_on() {
+	export http_proxy=http://127.0.0.1:7890
+	export https_proxy=$http_proxy
+}
+
+function proxy_off() {
+	unset http_proxy https_proxy
+}
+
+function proxy_status() {
+	echo http: $http_proxy
+	echo https: $https_proxy
+}
