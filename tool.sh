fe35503b08910826eb8de30cca2e5c47e279a2bf
r1cardohj <2413302357@qq.com>
2025-06-21 00:41:31 +0800

    keep pure linux proxy settings

--- tool.sh
+++ tool.sh
@@ -1,4 +1,10 @@
-function proxy_on() {
+function lo_proxy_on() {
+	ip=127.0.0.1
+	export http_proxy=http://$ip:7890
+	export https_proxy=$http_proxy
+}
+
+function wsl_proxy_on() {
 	ip=$(cat /etc/resolv.conf |grep -oP '(?<=nameserver\ ).*')
 	export http_proxy=http://$ip:7890
 	export https_proxy=$http_proxy
